7f3237afbeaa5a5659ca23c727b2a09f4883c2aa
ElBroce <92594770+ElBroce@users.noreply.github.com>
2023-03-04 17:06:03 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
 export function hello () {
-    alert("Hello!");
+    const picDiv = document.getElementById('upload');
+    const input = document.querySelector('input');
+    input.addEventListener('change', updateImageDisplay);
+
+    input.click(); 
+
+    function updateImageDisplay() {
+    root.style.setProperty('--bg', "url(" + URL.createObjectURL(input.files[0]) + ")");
+    }
 }
